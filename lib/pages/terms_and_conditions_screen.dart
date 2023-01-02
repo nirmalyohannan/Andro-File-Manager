baca15bdd9fa9aee4ac35f1637cc05c1a33aa213
Nirmal Yohannan <nirmalyohannan.in@gmail.com>
2023-01-02 20:30:18 +0530

    -Changed Terms and conditions UI

--- lib/pages/terms_and_conditions_screen.dart
+++ lib/pages/terms_and_conditions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:androfilemanager/pages/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class TermsAndCondtionScreen extends StatefulWidget {
   const TermsAndCondtionScreen({super.key});
@@ -15,51 +16,68 @@ class _TermsAndCondtionScreenState extends State<TermsAndCondtionScreen> {
     return SafeArea(
         child: Scaffold(
       body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Text(
-              "Terms and Conditions",
-              style: TextStyle(fontSize: 55, fontWeight: FontWeight.w500),
-            ),
-            const SizedBox(
-              height: 60,
-            ),
-            const Text(
-                "The 'Andro File Manager' app requires storage permission from the user for the file manager functionalities.",
-                style: TextStyle(fontSize: 20)),
-            const Text(
-                "'Andro File Manager' app will not misuse the storage permission. The app respects the user privacy and does not collect or send any user data over internet. The app does not connect to any avaialable internet connection.",
-                style: TextStyle(fontSize: 20)),
-            Row(
-              children: [
-                Checkbox(
-                  checkColor: Colors.blue,
-                  value: checkBoxValue,
-                  onChanged: (value) {
-                    setState(() {
-                      checkBoxValue = value! ? true : false;
-                    });
-                  },
-                ),
-                const Text("Access storage",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w800))
-              ],
-            ),
-            Visibility(
-                visible: checkBoxValue,
-                child: ElevatedButton(
-                    onPressed: () async {
-                      await prefs.setBool("isFirstTime", true);
-                      // ignore: use_build_context_synchronously
-                      Navigator.pushAndRemoveUntil(context, MaterialPageRoute(
-                        builder: (context) {
-                          return const SplashScreen();
-                        },
-                      ), (route) => false);
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Lottie.asset('assets/lottie/terms_and_conditions.json',
+                  width: MediaQuery.of(context).size.width / 2),
+              const Text(
+                "Terms & Conditions",
+                style: TextStyle(fontSize: 55, fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              const Text(
+                  "The 'Andro File Manager' app requires storage permission from the user for the file manager functionalities.",
+                  style: TextStyle(fontSize: 20)),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                  "'Andro File Manager' app will not misuse the storage permission. The app respects the user privacy and does not collect or send any user data over internet. The app does not connect to any avaialable internet connection.",
+                  style: TextStyle(fontSize: 20)),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Switch(
+                    value: checkBoxValue,
+                    onChanged: (value) {
+                      setState(() {
+                        checkBoxValue = value ? true : false;
+                      });
                     },
-                    child: const Text("Continue")))
-          ],
+                  ),
+                  const Flexible(
+                    child: Text("I hereby accepts the Terms and Conditions",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w500)),
+                  )
+                ],
+              ),
+              const Spacer(),
+              Visibility(
+                  visible: checkBoxValue,
+                  child: ElevatedButton(
+                      onPressed: () async {
+                        await prefs.setBool("isFirstTime", true);
+                        // ignore: use_build_context_synchronously
+                        Navigator.pushAndRemoveUntil(context, MaterialPageRoute(
+                          builder: (context) {
+                            return const SplashScreen();
+                          },
+                        ), (route) => false);
+                      },
+                      child: const Text("Continue"))),
+              const SizedBox(
+                height: 30,
+              )
+            ],
+          ),
         ),
       ),
     ));
